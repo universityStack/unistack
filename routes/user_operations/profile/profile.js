0b35966b803e54476a5931d478a797ca2378cda4
Tolunay Güdük <guduktolunay@gmail.com>
2017-11-21 16:19:42 +0300

    upload tamamlandı

--- routes/user_operations/profile/profile.js
+++ routes/user_operations/profile/profile.js
@@ -9,17 +9,13 @@ router.use(fileUpload());
 
 router.post('/imageUpload', function (req,res) {
     if (!req.files)
-        return res.status(400).send('No files were uploaded.');
-
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+        return res.status(400).send('Hiçbir Dosya Yüklenmedi...');
     var sampleFile = req.files.sampleFile;
-
-    // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv(__dirname + 'public/img/filename.jpg', function(err) {
+    sampleFile.mv(__dirname + '/../../../public/img/filename.jpg', function(err) {
         if (err)
             return res.status(500).send(err);
 
-        res.send('File uploaded!');
+        res.send('Resim Yüklendi...!');
     });
 });
 

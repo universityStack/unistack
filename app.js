c443044286d05128f422d8ecf8f9905128e6ec05
Tolunay Güdük <guduktolunay@gmail.com>
2017-12-07 01:32:42 +0300

    try

--- app.js
+++ app.js
@@ -67,11 +67,12 @@ io.sockets.on("connection", function (socket) {
     socket.on('switchRoom', function(newroom){
 
 
-            socket.join(newroom);
-            socket.channel = newroom;
-        
-
-
+        if(socket.channel){
+            socket.leave(socket.channel);
+            global_variables.logla.error(socket.channel);
+        }
+        socket.join(newroom);
+        socket.channel = newroom;
     });
 
 

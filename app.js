00bdbb11a420ff2ac91305f21eba87efe5f7fefc
Tolunay Güdük <guduktolunay@gmail.com>
2017-12-07 02:22:42 +0300

    try

--- app.js
+++ app.js
@@ -73,6 +73,7 @@ io.sockets.on("connection", function (socket) {
         }
         socket.join(newroom);
         socket.channel = newroom;
+        global_variables.logla.error(" channel"+socket.channel);
     });
 
 

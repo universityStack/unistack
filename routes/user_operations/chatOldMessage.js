0675f30f9ca1910400756223a451967ad96179d0
Tolunay Güdük <guduktolunay@gmail.com>
2017-12-12 19:09:23 +0300

    guncelle

--- routes/user_operations/chatOldMessage.js
+++ routes/user_operations/chatOldMessage.js
@@ -5,17 +5,31 @@ var router = express.Router();
 
 router.get('/',function (req,res) {
     rift = req.headers.rift;
+
+     odaListesi = {
+        1 : req.headers.oda1,
+        2: req.headers.oda2,
+        3 : req.headers.oda3,
+        4: req.headers.oda4
+    }
+
     altLimit= (rift-1)*50;
     ustLimit = rift*50;
-    db.query('select * from chat limit '+altLimit+','+ustLimit, function (err,data) {
-        if(err){
-            res.send({code:400,message:"db hatası"});
-            console.log(err);
-        }
-        else{
-            res.send(data);
-        }
-    });
+    for(var i = 1; i <= 4; i++){
+        db.query('select * from chat where gonderilen = "'+ odaListesi[i] +'" limit '+altLimit+','+ustLimit, function (err,data) {
+            if(err){
+                res.send({code:400,message:"db hatası",err});
+                console.log(err);
+            }
+            else{
+                odaListesi[i] = data;
+                console.log(odaListesi);
+            }
+        });
+    }
+
+    res.send(odaListesi);
+
 });
 
 
